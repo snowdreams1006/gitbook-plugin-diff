61842c13e69b15062c65f462ffd26f1ff735bf88
snowdreams1006 <snowdreams1006@163.com>
2020-03-19 13:49:02 +0800

    arr

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ module.exports = {
                 if(!method){
                     method = pluginMethod;
                 }
-                
+
                 var pluginOptions = pluginConfig.options;
                 var blockOptions = blockConfig.kwargs.options;
                 var options = blockOptions;
@@ -43,66 +43,93 @@ module.exports = {
                 if (textArr.length != 2){
                     throw new Error('Only Support 2 code block');
                 }
-                var oldArr = textArr[0];
-                var newArr = textArr[1];
-                
-                var showDiffCode = "\n",diff;
+                var oldStr = textArr[0];
+                var newStr = textArr[1];
+                var showDiffCode = "";
+                var diff;
+                var markdownCode = "";
                 switch (method) {
                     case "diffChars":
                         options = Object.assign({}, (options || {}),{"ignoreCase":false});
-                        diff = Diff.diffChars(oldArr, newArr,options);
+                        diff = Diff.diffChars(oldStr, newStr,options);
                       break; 
                     case "diffWords":
                         options = Object.assign({}, (options || {}),{"ignoreCase":false});
-                        diff = Diff.diffWords(oldArr, newArr,options);
+                        diff = Diff.diffWords(oldStr, newStr,options);
                         break; 
                     case "diffWordsWithSpace":
                         options = Object.assign({}, (options || {}),{"ignoreCase":false});
-                        diff = Diff.diffWordsWithSpace(oldArr, newArr,options);
+                        diff = Diff.diffWordsWithSpace(oldStr, newStr,options);
                         break; 
                     case "diffLines":
-                        // TODO
                         options = Object.assign({}, (options || {}),{"ignoreCase":false,"newlineIsToken":false});
-                        diff = Diff.diffLines(oldArr, newArr,options);
+                        diff = Diff.diffLines(oldStr, newStr,options);
                         break; 
                     case "diffTrimmedLines":
                         options = Object.assign({}, (options || {}),{"ignoreCase":false,"newlineIsToken":false});
-                        diff = Diff.diffTrimmedLines(oldArr, newArr,options);
+                        diff = Diff.diffTrimmedLines(oldStr, newStr,options);
                         break; 
                     case "diffSentences":
                         options = Object.assign({}, (options || {}),{"ignoreCase":false,"newlineIsToken":false});
-                        diff = Diff.diffSentences(oldArr, newArr,options);
+                        diff = Diff.diffSentences(oldStr, newStr,options);
                         break; 
                     case "diffCss":
                         options = Object.assign({}, (options || {}),{"ignoreCase":false,"newlineIsToken":false});
-                        diff = Diff.diffCss(oldArr, newArr,options);
+                        diff = Diff.diffCss(oldStr, newStr,options);
                         break; 
                     case "diffJson":
                         options = Object.assign({}, (options || {}),{"ignoreCase":false,"newlineIsToken":false});
                         if(Object.prototype.toString.call(oldStr) === '[object String]'){
                             oldStr = JSON.parse(oldStr);
                         }
-                        if(Object.prototype.toString.call(newArr) === '[object String]'){
-                            newArr = JSON.parse(newArr);
+                        if(Object.prototype.toString.call(newStr) === '[object String]'){
+                            newStr = JSON.parse(newStr);
                         }
-                        diff = Diff.diffJson(oldArr, newArr,options);
+                        diff = Diff.diffJson(oldStr, newStr);
                         break;
                     case "diffArrays":
                         if(Object.prototype.toString.call(oldStr) === '[object String]'){
                             oldStr = JSON.parse(oldStr);
                         }
-                        if(Object.prototype.toString.call(newArr) === '[object String]'){
-                            newArr = JSON.parse(newArr);
+                        if(Object.prototype.toString.call(newStr) === '[object String]'){
+                            newStr = JSON.parse(newStr);
                         }
-                        diff = Diff.diffArrays(oldArr, newArr);
-                        break; 
+                        diff = Diff.diffArrays(oldStr, newStr);
+                        break;
                 }
-                diff.forEach(function(part){
-                    var modifier = part.added ? "+ " : part.removed ? "- " : "  ";
-                    showDiffCode += modifier + part.value + '\n';
-                });
-
-                var markdownCode = '```diff'+(showDiffCode)+'```';
+                if(method == "diffJson"){
+                    diff.forEach(function(part){
+                        var modifier = part.added ? "+ " : part.removed ? "- " : "  ";
+                        part.value.split("\n").forEach(function(item){
+                            item = item.trim();
+                            if(item){
+                                showDiffCode += modifier + item + '\n';
+                            }
+                        });
+                    });
+                }else if (method == "diffArrays"){
+                    var diffArr = [];
+                    diff.forEach(function(part){
+                        var modifier = part.added ? "+   " : part.removed ? "-   " : "    ";
+                        part.value.forEach(function(item){
+                            item = item.trim();
+                            if(item){
+                                diffArr.push(modifier + item);
+                            }
+                        });
+                    });
+                    showDiffCode += '[  \n';
+                    diffArr.forEach(function(diffItem){
+                        showDiffCode += diffItem + "\n";
+                    });
+                    showDiffCode += ']'
+                }else{
+                    diff.forEach(function(part){
+                        var modifier = part.added ? "+ " : part.removed ? "- " : "  ";
+                        showDiffCode += modifier + part.value + '\n';
+                    });
+                }
+                markdownCode = '```diff\n'+showDiffCode+'```';
                 return new Promise(resolve => {
                     resolve(this.book.renderBlock('markdown', markdownCode) .then(function(html) {
                         return html;
